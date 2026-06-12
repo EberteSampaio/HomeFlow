160b9e82cecf2a0db505bbba281ea448b4ffc04c
Eberte Sampaio <deveberte@gmail.com>
2026-06-12 09:14:57 -0300

    chore: update settings.py

--- homeflow/settings.py
+++ homeflow/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3==cab2$2hy_!#cg!#r(=qs+0ne2ch-0%g1gp2o&0@-vey#tyd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEVELOPMENT_ENVIRONMENT', False)
@@ -106,13 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', 'en-us')
+TIME_ZONE = os.getenv('TIME_ZONE', 'UTC')
+USE_I18N = os.getenv('USE_I18N', True)
+USE_TZ = os.getenv('USE_TZ', True)
 
 
 # Static files (CSS, JavaScript, Images)
